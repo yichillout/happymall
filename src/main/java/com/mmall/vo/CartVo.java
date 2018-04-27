057da10aaf30e4835c2c27b7540fea3d19b483c3
Yi Xie <jasper@Yis-MacBook-Pro.local>
2018-04-27 00:07:17 -0400

    added cart function

---
+++ src/main/java/com/mmall/vo/CartVo.java
@@ -0,0 +1,49 @@
+package com.mmall.vo;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * Created by jasper on 4/22/18.
+ */
+public class CartVo {
+
+    List<CartProductVo> cartProductVoList;
+    private BigDecimal cartTotalPrice;
+    private Boolean allChecked; //是否都以勾选
+    private String imageHost;
+
+    public List<CartProductVo> getCartProductVoList() {
+        return cartProductVoList;
+    }
+
+    public void setCartProductVoList(List<CartProductVo> cartProductVoList) {
+        this.cartProductVoList = cartProductVoList;
+    }
+
+    public BigDecimal getCartTotalPrice() {
+        return cartTotalPrice;
+    }
+
+    public void setCartTotalPrice(BigDecimal cartTotalPrice) {
+        this.cartTotalPrice = cartTotalPrice;
+    }
+
+    public Boolean getAllChecked() {
+        return allChecked;
+    }
+
+    public void setAllChecked(Boolean allChecked) {
+        this.allChecked = allChecked;
+    }
+
+    public String getImageHost() {
+        return imageHost;
+    }
+
+    public void setImageHost(String imageHost) {
+        this.imageHost = imageHost;
+    }
+}
